f582f5f1b307abf134b841e5608f137212f9c821
Анастасия Е <144812664+flowereen@users.noreply.github.com>
2023-10-29 20:04:52 +0300

    Create 18.8.19 (HW-03).py

---
+++ 18.8.19 (HW-03).py
@@ -0,0 +1,13 @@
+price = 0
+tickets = (int(input("Введите количество билетов:")))
+for age in range (tickets):
+    age = (int(input("Введите ваш возраст:")))
+    if age < 18:
+        price += 0
+    elif age >= 18 and age <= 25:
+        price += 990
+    elif age > 25:
+        price += 1390
+if tickets > 3:
+    price -= price / 100 * 10
+print ("Стоимость билетов:", price)
